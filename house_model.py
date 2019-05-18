ce08d6ec2803247c0a65cfb555813f006451ce97
Ryan Kladar <rkladar@advmicrogrid.com>
2019-05-18 10:05:36 -0700

    initial code commit

---
+++ house_model.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+import darksky
+import pendulum
+
+
+class HouseModel(object):
+    def __init__(self):
+
+        self.timestep = pendulum.parse(0)
+        self.indoor_temp = 22
+        self.outdoor_temp = 15
+        self.weather = None
+
+    def time_stepper(self):
+
+        self.timestep = self.timestep.add(minutes=1)
+
+    def set_outdoor_temp(self):
+
+        # use darksky method
+
+    def set_indoor_temp(self):
+
+        # define mathematical adjustment
+
+    def set_weather(self):
+
+        # use darksky method
+
+    def set_window_state(self):
+
+        if
+
